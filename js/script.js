9ca0e618f06f739247ba2b3d5111245c7136392f
nedduc <marinacudden@gmail.com>
2021-10-24 17:48:06 +0000

    Click on and off color boxes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-$('button').mouseenter(function() {
-	$(this).removeClass('makeRed').addClass('makeBlue');
-});
-
-$('button').mouseleave(function() {
-	$(this).removeClass('makeBlue').addClass('makeRed');
-});
+$(document).ready(function(){
+    $(".box").on("click", function(){
+        /**
+         * when we click on an element that has
+         * a 'box' class, this event will be fired.
+         */
+        var classNames = $(this).attr("class").split(" ");
+        var boxClass = classNames[0];
+        var className = classNames[1];
+        if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+            //If this object is already red, turn it black
+            $("." + className).css("background-color", "#000");
+        } else {
+            //Else turn all elements with a box xlass black
+            //and then change the color of all the elements
+            //with the same class name as the clicked element to
+            //red.
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
+        });
+    });
